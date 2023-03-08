2d95ad45a2ad923a002f7e7888d6811321117828
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-08 12:18:00 +0000

    Issue model changes

--- db/migrate/20230308115012_add_columns_to_issues.rb
+++ db/migrate/20230308115012_add_columns_to_issues.rb
@@ -2,5 +2,6 @@ class AddColumnsToIssues < ActiveRecord::Migration[7.0]
   def change
     add_column :issues, :title, :string
     add_column :issues, :code_block, :text
+    change_column :issues, :description, :text
   end
 end
