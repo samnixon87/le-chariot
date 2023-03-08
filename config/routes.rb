c15ef7ef7edc0d552fbbffb72447187dc66bc23b
Axel Grubba <admin@axelgrubba.com>
2023-03-08 15:32:38 +0000

    added multi stage form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "/chat" => "pages#chat"
+  get "/chatgpt" => "pages#chatgpt"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
