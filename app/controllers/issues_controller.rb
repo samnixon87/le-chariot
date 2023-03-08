0724eb94560d7f5552c27d0dfb14f19bc4cf1f4e
Willem Duyck <duyckwillem@gmail.com>
2023-03-08 15:11:47 +0000

    Updated issues controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,19 @@
 class IssuesController < ApplicationController
-  def create
+  def new
     @channel = Channel.find(params[:channel_id])
     @issue = Issue.new(issue_params)
+  end
+
+  def create
+    @user = current_user
+    @issue = Issue.new(issue_params)
+    @issue.user_id = current_user.id
+    @issue.user_id = User.find(params[:user_id]).id
+    if @issue.save
+      redirect_to user_path(current_user), notice: "Issue successful!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -17,6 +29,7 @@ class IssuesController < ApplicationController
   private
 
   def issue_params
-    params.require(:issue).permit(:content, :description, :rating, :user_id, :created_at, :updated_at, :channel_id)
+    params.require(:issue).permit(:content, :description, :rating, :user_id, :created_at,
+                                  :updated_at, :channel_id, :title, :code_block)
   end
 end
