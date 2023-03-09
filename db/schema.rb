06e7945156f672fad8f728fbd7fa37a0451d1dde
Willem Duyck <duyckwillem@gmail.com>
2023-03-09 12:44:25 +0000

    Adding pin ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_115012) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_114850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_115012) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chatroom_messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "channel_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_chatroom_messages_on_channel_id"
+    t.index ["user_id"], name: "index_chatroom_messages_on_user_id"
   end
 
   create_table "chatrooms", force: :cascade do |t|
@@ -46,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_115012) do
     t.bigint "issue_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "pinned", default: false
     t.index ["issue_id"], name: "index_messages_on_issue_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -81,6 +92,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_115012) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chatroom_messages", "channels"
+  add_foreign_key "chatroom_messages", "users"
   add_foreign_key "issues", "channels"
   add_foreign_key "issues", "users"
   add_foreign_key "messages", "issues"
