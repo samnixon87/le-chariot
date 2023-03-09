0689c1b9913cb9d800fc46bfd7534d7fe1fad147
Axel Grubba <admin@axelgrubba.com>
2023-03-09 12:59:00 +0000

    updated form

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,14 +1,16 @@
 class IssuesController < ApplicationController
   def new
-    @channel = Channel.find(params[:channel_id])
-    @issue = Issue.new(issue_params)
+    @user = current_user
+    @issue = Issue.new
   end
 
   def create
+    # @channel = Channel.find(params[:channel_id])
+    # @channels = Channel.all
     @user = current_user
     @issue = Issue.new(issue_params)
-    @issue.user_id = current_user.id
-    @issue.user_id = User.find(params[:user_id]).id
+    @issue.user = current_user
+    # @issue.channel = @channel
     if @issue.save
       redirect_to user_path(current_user), notice: "Issue created successfully!"
     else
@@ -20,10 +22,6 @@ class IssuesController < ApplicationController
     @channel = Channel.find(params[:channel_id])
     @issue = Issue.find(params[:id])
     @message = Message.new
-    respond_to do |format|
-      format.html
-      format.json
-    end
   end
 
   def index
@@ -34,7 +32,6 @@ class IssuesController < ApplicationController
   private
 
   def issue_params
-    params.require(:issue).permit(:description, :rating, :user_id, :created_at,
-                                  :updated_at, :channel_id, :title, :code_block)
+    params.require(:issue).permit(:description, :title, :rating, :channel_id, :user_id, :code_block)
   end
 end
