f39538b0035244c5f32a999aee1f0f9a13ca3337
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-10 14:34:16 +0000

    Sort schema

--- db/schema.rb
+++ db/schema.rb
@@ -39,15 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_123950) do
     t.index ["user_id"], name: "index_issues_on_user_id"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.bigint "message_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["message_id"], name: "index_likes_on_message_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "messages", force: :cascade do |t|
     t.integer "rating"
     t.string "content"
