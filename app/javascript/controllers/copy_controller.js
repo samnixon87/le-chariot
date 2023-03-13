cd1803527650433fd78c8271688a6f44a2e09db5
Willem Duyck <duyckwillem@gmail.com>
2023-03-13 17:20:17 +0000

    copy paste button on chatbot

---
+++ app/javascript/controllers/copy_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="copy"
+export default class extends Controller {
+  static targets = ["text", "button"]
+  connect() {
+  }
+  copyText() {
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(this.textTarget.innerText);
+
+    // Alert the copied text
+    alert("Copied the text: " + this.textTarget.innerText);
+  }
+}
