a9c5afcea8f907810ddfcb577d5446c6a54ef8ca
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-14 09:57:42 +0000

    Add validations to issues and messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_134010) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_144637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_134010) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
+    t.text "content"
     t.bigint "user_id", null: false
     t.bigint "issue_id", null: false
     t.datetime "created_at", null: false
