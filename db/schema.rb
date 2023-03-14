046183268a1383402eefb80a88aa0f527421b1f5
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-14 15:52:13 +0000

    Update seeds. Add images and descriptions from Github. Add images to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_144637) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_151349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -143,6 +143,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_144637) do
     t.string "uid"
     t.string "name"
     t.string "username"
+    t.string "image"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
