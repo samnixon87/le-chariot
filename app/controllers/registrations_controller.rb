83ed26e3cd3d69a147324f1791a21fa4bd880e9a
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-16 16:37:29 +0000

    Update params to include nickname

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :image, :description, :password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :image, :nickname, :description, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :username, :email, :image, :description, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :username, :email, :image, :nickname, :description, :password, :password_confirmation, :current_password)
   end
 end
